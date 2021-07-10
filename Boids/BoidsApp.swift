b3a58e4db2be80e4a3b17e96a668f9dc26bedca9
Eric Groom <bcgroom@gmail.com>
2021-07-10 13:35:14 -0700

    Initial Commit

---
+++ Boids/BoidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoidsApp.swift
+//  Boids
+//
+//  Created by Eric Groom on 7/10/21.
+//
+
+import SwiftUI
+
+@main
+struct BoidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
